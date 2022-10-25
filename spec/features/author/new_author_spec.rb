268d030905e3fe1e3864d0fb492b1a637de73751
Jan Fehse <jan.fehse@student.hpi.uni-potsdam.de>
2022-10-25 19:45:29 +0200

    Test in the right place

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "Model has do be saved" do
+    visit new_author_path
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    page.fill_in 'author[first_name]', with: 'Not_found'
+    page.fill_in 'author[homepage]', with: 'This_is_a_website'
+    find('input[type="submit"]').click
+
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -14,12 +14,4 @@ RSpec.describe Author, type: :model do
     expect(author.name).to eq("Alan Turing")
   end
   
-  it "Model has do be saved" do
-    visit new_author_path
-    page.fill_in 'author[last_name]', with: 'Dijkstra'
-    page.fill_in 'author[first_name]', with: 'Not_found'
-    page.fill_in 'author[homepage]', with: 'This_is_a_website'
-    find('input[type="submit"]').click
-
-  end
 end
